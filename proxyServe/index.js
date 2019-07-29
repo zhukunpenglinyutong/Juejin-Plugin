ec315e53598d678af92d58c584f9c272af591921
zhukunpenglinyutong <270750933@qq.com>
2019-07-29 09:42:57 +0800

    第3次提交（09:42）| 修改一些内容

--- proxyServe/index.js
+++ proxyServe/index.js
@@ -2,11 +2,11 @@ const Koa = require('koa');
 const app = new Koa();
 const Router = require('koa-router')
 const router = new Router
-const bodyBody = require('koa-body') // 解析请求体
-const cors = require('koa2-cors') // 解决跨域
-const { getData } = require('./conller')
+const bodyBody = require('koa-body')
+const cors = require('koa2-cors')
+const { getData } = require('./logic')
 
-app.use(cors())
+app.use(cors()) // 解决跨域问题
 app.use(bodyBody()) // 解析 请求body中的 JSON
 
 // 路由部分
@@ -16,11 +16,6 @@ router.post('/proxy', async (ctx) => {
     // 处理函数
     ctx.body = data
 })
-
-
-
-
-
 
 app.use(router.routes()) // 注册路由
 app.listen(3000, () => console.log(`http://localhost:3000`)) // 启动Node服务
