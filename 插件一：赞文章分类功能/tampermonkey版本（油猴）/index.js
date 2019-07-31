8d4545f2855710ae31ef3c86cbd3b88b740690f2
zhukunpenglinyutong <270750933@qq.com>
2019-07-31 21:35:26 +0800

    第12次提交（21:35）| 增加油猴端重写版本代码，删除谷歌浏览器代理层代码

--- 插件一：赞文章分类功能/tampermonkey版本（油猴）/index.js
+++ 插件一：赞文章分类功能/tampermonkey版本（油猴）/index.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         掘金插件
 // @namespace    https://github.com/zhukunpenglinyutong/Juejin-Plugin
-// @version      0.1
-// @description  掘金收藏插件 - 更快速的完善内容
+// @version      1.0
+// @description  掘金插件 - 赞文章分类功能
 // @author       朱昆鹏
 // @match        *://juejin.im/user/*
 // @require https://cdn.bootcss.com/axios/0.19.0-beta.1/axios.min.js
 // @grant        none
 // ==/UserScript==
 
+let globalData = null; // 全局数据
 
 // 实现一个简单的路由监听（监听跳转是否是主页）
 window.addEventListener('load', function (e) {
@@ -43,10 +44,17 @@ function createMoreItem() {
 function eventBus() {
     let jSort = document.getElementById('jSort')
     jSort.addEventListener('click', async event => {
-        let dataReq = new DataReq()
-        let userId = location.pathname.match(/\w{24}/)[0]
-        let res = await dataReq.getData(userId) // 获取数据
-        createDOM(res) // 创建展示数据的DOM层
+        let dialogId = document.getElementById('dialogId')
+
+        if (dialogId) { // 已经点击过分类查看了，只是隐藏到了，这里避免重复拿取，浪费性能
+          dialogId.style.display = 'inline';
+        } else {
+          let dataReq = new DataReq()
+          let userId = location.pathname.match(/\w{24}/)[0]
+          let res = await dataReq.getData(userId) // 获取数据
+          createDOM(res) // 创建展示数据的DOM层
+        }
+
     })
 }
 
@@ -59,7 +67,7 @@ function eventBus() {
 // axios配置
 let instance = axios.create({
     baseURL: 'https://user-like-wrapper-ms.juejin.im/v1/user/',
-    timeout: 15000,
+    timeout: 30000,
     headers: { "X-Juejin-Src": "web" }
 });
 
@@ -188,21 +196,124 @@ class DataReq {
 
 
 // ============================================================================================
-// ========================== DOM生成层，根据数据生成对应的DOM和交互  ==============================
+// ========================= DOM生成层，根据数据生成对应的弹出层和交互  =============================
 // ============================================================================================
 
 const createDOM = (res) => {
-    console.log('数据是：', res)
+    globalData = res
+
+    // 响应式监听属性，响应点击标签时候的视图切换
+    let observeData = {
+        select: '' // 当前选中的分类名称
+    }
+
+    renderBasic() // 渲染弹出框基本结构
+    defineReactive(observeData, 'select', observeData.select) // 监听响应式
+    observeData.select = res[0].name // 初始化，渲染第一个分类中的数据
+    createDOMEventBus(observeData) // 启动事件监听
+}
+
+// 渲染弹出框基本结构
+function renderBasic() {
+
+    let dialogMianItem = '' // 根据数据，生成底部导航栏结构
+    globalData.forEach( item => {
+      dialogMianItem += `<div class="dialog-mian-item" style="position: relative;margin-right: 25px;">
+                <button class="dialogBtn" style="padding: 9px 15px;font-size: 12px;border-radius: 3px;min-width: 100px;">${item.name}</button>
+                <sup style="position: absolute;top: 0;right: 10px;transform: translateY(-50%) translateX(100%);background-color: #f56c6c;border-radius: 10px;color: #fff;display: inline-block;font-size: 12px;height: 18px;line-height: 18px;padding: 0 6px;text-align: center;white-space: nowrap;">
+                  ${item.num}
+                </sup>
+              </div>`
+    })
+
 
     // 创建弹出层
-    // 创建 分类按钮
-    // 创建 搜索
-    // 创建 展示
+    let div = document.createElement('div')
+    div.id = 'dialogId'
+    // 模板中展示的内容
+    div.innerHTML = `
+      <div class="dialog"
+        style="margin: 15vh auto 50px;width: 60vw;height: 600px;background: #fff;opacity: 1;border-radius: 2px;box-shadow: 0 1px 3px rgba(0,0,0,.3);box-sizing: border-box;">
+        <div class="dialog-header" style="padding: 20px 20px 10px;line-height: 24px;font-size: 18px;color: #303133;margin-bottom: 15px;display: flex;justify-content: space-between;">
+          <div>文章分类 <input style="margin-left: 10px;" disabled value="搜索功能8.1日开放" /> </div>
+          <div id="dialog-close" style="cursor: pointer;">X</div>
+        </div>
+        <div class="dialog-mian">
+          <div class="dialog-mian" style="padding: 0 22px;">
+            <div class="dialog-mian-lang" style="display: flex;justify-content: space-between;padding-top: 12px;overflow-y: auto;">${dialogMianItem}</div>
+            <div class="dialog-content" style="margin-top: 10px;height: 430px;overflow-y: auto;">
 
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+    let body = document.querySelector('body')
+    div.style = `position: fixed;height: 100vh;width: 100vw;top: 0;left: 0;z-index: 1000;`
+    body.appendChild(div)
+}
+
+// 生成详细文章列表
+function createDialogContent(selectName) {
+
+  let randerData = globalData.filter( item => item.name === selectName)[0].data
+  let dialogContent = document.querySelector('.dialog-content')
+
+  let dialogContentText = ''; // 具体一行行内容
+  // 当前点击展示的内容
+  randerData.forEach(item => {
+      let aStr = ''
+      item.tags.forEach( tag => {
+        aStr += `<a href="javascript:;">${tag} · </a>`
+      })
+      dialogContentText += `
+                <div class="dialog-content_item" style="margin: 18px 0px;border-bottom: 1px solid rgba(178,186,194,.15);">
+                   <div class="meta-row">
+                      <a href="${item.objectId}">${item.username}</a> · ${aStr}
+                   </div>
+                   <div class="title" style="margin-top: 6px;"><a href="${item.originalUrl}" style="font-size: 16px;color: #2E3135;">${item.title}</a></div>
+                </div>`
+  })
+
+  dialogContent.innerHTML = dialogContentText
+}
+
+// 事件总线
+function createDOMEventBus (observeData) {
+    // 点击切换事件（采用事件冒泡，进行捕获）
+    let btns = document.querySelector('.dialog-mian-lang')
+    btns.addEventListener('click', function (e) {
+      console.log(e)
+      if (e.target.nodeName === 'BUTTON') observeData.select = e.target.textContent;
+    })
+
+    // 点击关闭按钮
+    let dialogClose = document.getElementById('dialog-close')
+    let dialogId = document.getElementById('dialogId')
+    dialogClose.addEventListener('click', function () {
+        console.log('触发')
+       dialogId.style.display = 'none'
+    })
 }
 
 
+// ============================================================================================
+// ========================== 实现简单数据监听，用来简化DOM操作（朱昆鹏）  ===========================
+// ============================================================================================
 
-// ============================================================================================
-// ========================== 实现简单MVVM，用来简化DOM操作（朱昆鹏）  =============================
-// ============================================================================================
+// 数据劫持
+function defineReactive (obj, key, value) {
+    Object.defineProperty(obj, key, {
+        enumerable: true,
+        configurable: true,
+
+        get () {
+           return value
+        },
+
+        set (newValue) {
+           value = newValue
+           createDialogContent(value) // 更新视图 (生成详细文章列表)
+        }
+    })
+}
